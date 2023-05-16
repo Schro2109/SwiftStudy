9b70a5d04398d054f6e933a4e1a70c61ccb83e2c
Schro2109 <bloodparty297@gmail.com>
2023-05-16 14:26:45 +0900

    add: 프로젝트 생성

---
+++ SwiftStudy/Controller/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  SwiftStudy
+//
+//  Created by Schro on 2023/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ SwiftStudy/View/MainView.swift
@@ -0,0 +1,17 @@
+//
+//  MainView.swift
+//  SwiftStudy
+//
+//  Created by Schro on 2023/05/16.
+//
+
+import UIKit
+
+class MainView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
