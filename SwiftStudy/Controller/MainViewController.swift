93b7fac7ba68906917235043a7a0938741660754
Schro2109 <bloodparty297@gmail.com>
2023-05-18 13:53:35 +0900

    add: 코드베이스로 UITextField를 출력 시도, Constraints 오류로 인해 실패

--- SwiftStudy/Controller/MainViewController.swift
+++ SwiftStudy/Controller/MainViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
+        self.view.backgroundColor = .red
+        view = MainView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+//        view = MainView()
         super.viewDidLoad()
     }
+    
 }
 
--- SwiftStudy/View/MainView.swift
+++ SwiftStudy/View/MainView.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 class MainView: UIView {
+    var textField: UITextField?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        uiSetup()
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        uiSetup()
+    }
+    private func uiSetup() {
+        setViews()
+        setConstraints()
+        addView()
+    }
+    private func setViews() {
+        textField = UITextField().then {
+            $0.layer.backgroundColor = CGColor(red: 255.0, green: 255.0, blue: 0.0, alpha: 1.0)
+            $0.text = "TEST"
+            $0.textColor = .white
+        }
+    }
+    private func setConstraints() {
+//        let safeArea = super.safeAreaLayoutGuide.snp
+        
+        guard let textField = self.textField else { return }
+        textField.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(0)
+            make.leading.equalTo(0)
+            make.trailing.equalTo(0)
+            make.height.equalTo(100)
+        }
+    }
+    private func addView() {
+        guard let textField = self.textField else { return }
+        self.addSubview(textField)
     }
 }
